2c009cd3a5536370c65f7057767eb7646adaf97c
Happy <happy@gmail.com>
2018-04-24 19:36:11 +0800

    add username and password confirm

--- app/src/main/java/com/example/light/myapplication/Loginactivity.java
+++ app/src/main/java/com/example/light/myapplication/Loginactivity.java
@@ -5,9 +5,16 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+import org.json.JSONObject;
+
 public class Loginactivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +24,23 @@ public class Loginactivity extends AppCompatActivity {
         if(actionBar != null ){
             actionBar.hide();
         }
-        TextView tv=(TextView) findViewById(R.id.wancheng);
-        tv.setOnClickListener(new View.OnClickListener() {
+        username = (EditText)findViewById(R.id.login_username);
+        password = (EditText)findViewById(R.id.login_pwd);
+
+
+        TextView login=(TextView) findViewById(R.id.wancheng);
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RequestBody requestLogin = new FormBody.Builder()
+                        .add("username",username.getText().toString())
+                        .add("password",password.getText().toString()).
+                                build();
+                String url = "/user";
+                OkhttpConnection okhttpConnection = new OkhttpConnection();
+                JSONObject jsonObject = okhttpConnection.getData(url);
+
                 Intent intent = new Intent(Loginactivity.this,MainActivity.class);
                 startActivity(intent);
             }

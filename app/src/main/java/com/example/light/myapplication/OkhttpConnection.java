1414348ecccbc8bf70c27ea2a59e62880f1345bb
Happy <happy@gmail.com>
2018-04-24 19:33:41 +0800

    add Message getImage

--- app/src/main/java/com/example/light/myapplication/OkhttpConnection.java
+++ app/src/main/java/com/example/light/myapplication/OkhttpConnection.java
@@ -1,11 +1,15 @@
 package com.example.light.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Message;
 import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -22,8 +26,9 @@ import okhttp3.ResponseBody;
  */
 
 public class OkhttpConnection {
-    private String baseurl;
-    private String result;
+    public String baseurl = "https://172.17.0.16";
+    public String result;
+    public RequestBody requestBody;
 
     public void setResult(String re){
         this.result = re;
@@ -31,14 +36,19 @@ public class OkhttpConnection {
     public String getResult(){
         return this.result;
     }
+
+    public void setRequestBody(RequestBody requestBody){this.requestBody = requestBody;}
+    public RequestBody getRequestBody(){return this.requestBody;}
+
     public OkhttpConnection(String baseurl){
         this.baseurl = baseurl;
     }
+    public OkhttpConnection(){}
 
-    public JSONObject postData(final String url,JSONObject postjson){
+    public JSONObject postData(final String url){
         try{
             OkHttpClient client = new OkHttpClient();
-            RequestBody requestBody= new FormBody.Builder().build();
+            RequestBody requestBody=getRequestBody();
             Request request = new Request.Builder().url(baseurl+url).post(requestBody).build();
             Call call = client.newCall(request);
             call.enqueue(new Callback() {
@@ -97,5 +107,31 @@ public class OkhttpConnection {
             e.printStackTrace();
         }
         return resp;
+    }
+
+    public Message getImage(String url){
+        final Message msg = new Message();
+        try{
+            OkHttpClient okHttpClient = new OkHttpClient();
+            final Request request = new Request.Builder()
+                    .url(baseurl+url)
+                    .build();
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.i("okhttp","fail"+e);
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    InputStream inputStream = response.body().byteStream();
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    msg.obj = bitmap;
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return msg;
     }
 }

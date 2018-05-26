6489976d0447fbe798e5fcdc8d078c6582db195d
Happy <happy@gmail.com>
2018-05-26 12:45:21 +0800

    add /login and write userInfo

--- app/src/main/java/com/example/light/myapplication/Loginactivity.java
+++ app/src/main/java/com/example/light/myapplication/Loginactivity.java
@@ -1,5 +1,6 @@
 package com.example.light.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +9,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 
@@ -30,9 +32,7 @@ public class Loginactivity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.login_username);
         password = (EditText)findViewById(R.id.login_pwd);
 
-
         TextView login=(TextView) findViewById(R.id.wancheng);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,14 +46,50 @@ public class Loginactivity extends AppCompatActivity {
                 public void run() {
                     // TODO Auto-generated method stub
                     OkhttpConnection okhttpConnection = new OkhttpConnection();
+                    JSONObject json = new JSONObject();
+                    try{
+                        json.put("username",username.getText().toString());
+                        json.put("password",password.getText().toString());
+                    }catch(JSONException e){
+                        e.printStackTrace();
+                    }
+
                     try {
-                        String json = okhttpConnection.get("/hello");
-                        Log.i("Baobaoyue",json);
+                        String result = okhttpConnection.post("/login",json.toString());
+                        Log.i("Baobaoyue",result);
+                        try{
+                            JSONObject out = new JSONObject(result);
+                            String token = out.getString("token");
+                            String status = out.getString("status");
+                            if ( status.equals("true")){
+                                try{
+                                    String userInfo = okhttpConnection.post("/login",json.toString(),token);
+                                    Log.i("Baobaoyue2",userInfo);
+                                    writeFiles(userInfo);
+                                }catch(IOException e){
+                                    e.printStackTrace();
+                                }
+                                Intent intent = new Intent(Loginactivity.this,MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }catch(JSONException e){
+                            e.printStackTrace();
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             };
         });
+    }
+
+    private void writeFiles(String string){
+        try{
+            FileOutputStream outputStream = openFileOutput("UserInfo", Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
